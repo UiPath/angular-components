4d3359e329fc036215fcd74b7a468e5a26c314ae
Sergiu Bologa <sergiu.bologa@uipath.com>
2019-02-20 12:10:15 +0200

    Initial commit and first directive

---
+++ projects/angular-components/src/lib/directives/ui-ng-let/ui-ng-let.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+import { UiNgLetDirective } from './ui-ng-let.directive';
+
+@NgModule({
+    declarations: [UiNgLetDirective],
+    exports: [UiNgLetDirective]
+  })
+  export class UiNgLetModule { }
