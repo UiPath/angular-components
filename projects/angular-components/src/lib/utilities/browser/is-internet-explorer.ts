2067bffee0b8a0261e86e28934420622b0e4bc05
Vlad Jerca <vlad.jerca@gmail.com>
2019-02-28 14:56:52 +0200

    browser(is-internet-explorer): determine if the user agent is IE

---
+++ projects/angular-components/src/lib/utilities/browser/is-internet-explorer.ts
@@ -0,0 +1,18 @@
+export /**
+ * Determines if the current agent is Internet Explorer.
+ *
+ * @returns {boolean}
+ */
+const isInternetExplorer = (): boolean => {
+    const userAgent = window.navigator.userAgent;
+
+    // IE 10 or older => return version number
+    const msie = userAgent.indexOf('MSIE ');
+    if (msie > -1) { return true; }
+
+    // IE 11 => return version number
+    const trident = userAgent.indexOf('Trident/');
+    if (trident > -1) { return true; }
+
+    return false;
+};
