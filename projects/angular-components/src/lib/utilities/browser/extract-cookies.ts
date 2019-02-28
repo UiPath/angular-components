0c91ea033d229962d17461bfc7ef41734e980ed1
Vlad Jerca <vlad.jerca@gmail.com>
2019-02-28 14:57:59 +0200

    browser(extract-cookies): extract all the available cookie values

---
+++ projects/angular-components/src/lib/utilities/browser/extract-cookies.ts
@@ -0,0 +1,12 @@
+export /**
+ * Returns a map with all the cookie KV pairs.
+ *
+ * @returns {Record<string, string>}
+ */
+const extractCookies = (): Record<string, string> =>
+    document.cookie.split('; ')
+        .reduce((cookieMap, cookie) => {
+            const [key, value] = cookie.split('=');
+            cookieMap[key] = value;
+            return cookieMap;
+        }, {});
