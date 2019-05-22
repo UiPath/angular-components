91dbebef6587e692ef6e35db31d650950ffd0218
Vlad Jerca <vlad.jerca@gmail.com>
2019-05-22 22:10:58 +0300

    test(utilities): mark cursor property as helper

--- packages/apollo-angular-components/projects/angular/testing/src/utilities/event-generator.ts
+++ packages/apollo-angular-components/projects/angular/testing/src/utilities/event-generator.ts
@@ -12,33 +12,35 @@ export interface IDropEvent extends Event {
     };
 }
 
+// tslint:disable-next-line:max-line-length
+const CURSOR_IMG = 'url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAMAAAC6V+0/AAABLFBMVEUAAAD///8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAQECAgIKCgoNDQ0ODg4PDw8TExMVFRUeHh4hISEoKCgvLy89PT1QUFBWVlZaWlpiYmJoaGh3d3d5eXl9fX2IiIiXl5eZmZmmpqaoqKiqqqqrq6usrKywsLC1tbW4uLi8vLzBwcHU1NTc3Nzf39/o6Ojp6ens7Ozw8PD39/f4+Pj5+fn6+vr7+/v8/Pz9/f3///8jvPNLAAAAMnRSTlMAAAEGBxETFRodKDE/Q0ldXmZqeX1/hYeLjZaYmZqbnJ2ep6y0ur3JzNbi4+To7O38/mEAm4cAAADUSURBVHgBXdDlUoZAFMbxxcBu7MAQW8EHsbtDVOxgFX3u/x7EAfeFPTPny2/OhzN/YRhG0yQ6hZoU/nbQ34al4xgez9Cv4SgiHsMuo40b8hQTGt7yh+dw9Ms44QWmCziCkDLVK8zWlFHGX4wwV/uPwxl+kk+BW1/AOJGSZISpAn6Td5s7a1vBeI5DuGZy+PGOxS6rrVrhPfdwyRPPVC+lGB64C7t8xUAF+/xVNLbigUfLpsK67vkWUbW0zzf0KMxC9OLled3RsAEbK16zhqJ9piMP8gvd2SzacwFNVgAAAABJRU5ErkJggg==)';
+
 // @dynamic
 export class EventGenerator {
-    static cursor = {
+    private static _cursor = {
         ref: {} as HTMLElement,
         initialize: (parent?: HTMLElement) => {
             if (!parent) { return; }
-            EventGenerator.cursor.ref = document.createElement('div') as HTMLElement;
+            EventGenerator._cursor.ref = document.createElement('div') as HTMLElement;
 
-            EventGenerator.cursor.ref.style.position = 'absolute';
-            EventGenerator.cursor.ref.style.display = 'block';
-            EventGenerator.cursor.ref.style.left = '0px';
-            EventGenerator.cursor.ref.style.top = '0px';
-            EventGenerator.cursor.ref.style.width = '20px';
-            EventGenerator.cursor.ref.style.height = '20px';
-            // tslint:disable-next-line:max-line-length
-            EventGenerator.cursor.ref.style.background = 'url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAMAAAC6V+0/AAABLFBMVEUAAAD///8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAQECAgIKCgoNDQ0ODg4PDw8TExMVFRUeHh4hISEoKCgvLy89PT1QUFBWVlZaWlpiYmJoaGh3d3d5eXl9fX2IiIiXl5eZmZmmpqaoqKiqqqqrq6usrKywsLC1tbW4uLi8vLzBwcHU1NTc3Nzf39/o6Ojp6ens7Ozw8PD39/f4+Pj5+fn6+vr7+/v8/Pz9/f3///8jvPNLAAAAMnRSTlMAAAEGBxETFRodKDE/Q0ldXmZqeX1/hYeLjZaYmZqbnJ2ep6y0ur3JzNbi4+To7O38/mEAm4cAAADUSURBVHgBXdDlUoZAFMbxxcBu7MAQW8EHsbtDVOxgFX3u/x7EAfeFPTPny2/OhzN/YRhG0yQ6hZoU/nbQ34al4xgez9Cv4SgiHsMuo40b8hQTGt7yh+dw9Ms44QWmCziCkDLVK8zWlFHGX4wwV/uPwxl+kk+BW1/AOJGSZISpAn6Td5s7a1vBeI5DuGZy+PGOxS6rrVrhPfdwyRPPVC+lGB64C7t8xUAF+/xVNLbigUfLpsK67vkWUbW0zzf0KMxC9OLled3RsAEbK16zhqJ9piMP8gvd2SzacwFNVgAAAABJRU5ErkJggg==)';
+            EventGenerator._cursor.ref.style.position = 'absolute';
+            EventGenerator._cursor.ref.style.display = 'block';
+            EventGenerator._cursor.ref.style.left = '0px';
+            EventGenerator._cursor.ref.style.top = '0px';
+            EventGenerator._cursor.ref.style.width = '20px';
+            EventGenerator._cursor.ref.style.height = '20px';
+            EventGenerator._cursor.ref.style.background = CURSOR_IMG;
 
-            parent.appendChild(EventGenerator.cursor.ref);
+            parent.appendChild(EventGenerator._cursor.ref);
         },
         destroy: (parent?: HTMLElement) => {
             if (!parent) { return; }
 
-            parent.removeChild(EventGenerator.cursor.ref);
+            parent.removeChild(EventGenerator._cursor.ref);
         },
         update: (x: number, y: number) => {
-            EventGenerator.cursor.ref.style.left = `${x - 10}px`;
-            EventGenerator.cursor.ref.style.top = `${y - 10}px`;
+            EventGenerator._cursor.ref.style.left = `${x - 10}px`;
+            EventGenerator._cursor.ref.style.top = `${y - 10}px`;
         },
     };
 
@@ -123,7 +125,7 @@ export class EventGenerator {
             value: offsetY,
         });
 
-        this.cursor.update(offsetX, offsetY);
+        this._cursor.update(offsetX, offsetY);
         return event;
     }
 
@@ -139,7 +141,7 @@ export class EventGenerator {
             value: offsetY,
         });
 
-        this.cursor.update(offsetX, offsetY);
+        this._cursor.update(offsetX, offsetY);
         return event;
     }
 
