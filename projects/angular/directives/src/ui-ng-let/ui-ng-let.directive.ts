6a09a7be2492c8d109f54b13619f609bb5a3d338
Vlad Jerca <vlad.jerca@gmail.com>
2019-05-27 14:49:18 +0300

    directives: update api docs

--- projects/angular/directives/src/ui-ng-let/ui-ng-let.directive.ts
+++ projects/angular/directives/src/ui-ng-let/ui-ng-let.directive.ts
@@ -1,37 +1,58 @@
 import {
-    Directive,
-    Input,
-    OnInit,
-    TemplateRef,
-    ViewContainerRef,
+  Directive,
+  Input,
+  OnInit,
+  TemplateRef,
+  ViewContainerRef,
 } from '@angular/core';
 
+/**
+ * @ignore
+ */
 class NgLetContext {
   $implicit: any = null;
   ngLet: any = null;
 }
 
+/**
+ * A directive that allows declaration of streams inside the `template`.
+ * Similar to `*ngIf="source$ | async as source"`.
+ * `NgLet` works the same wa, the difference being that the content is rendered,
+ * even if the source has not yet been initialized.
+ *
+ * @export
+ */
 @Directive({
-// tslint:disable-next-line: directive-selector
+  // tslint:disable-next-line: directive-selector
   selector: '[ngLet]'
 })
 export class UiNgLetDirective implements OnInit {
   private _context = new NgLetContext();
 
-    @Input()
-    set ngLet(value: any) {
-        this._context.$implicit = this._context.ngLet = value;
-    }
+  /**
+   * The context bound to the decorated area.
+   *
+   */
+  @Input()
+  set ngLet(value: any) {
+    this._context.$implicit = this._context.ngLet = value;
+  }
 
-    constructor(
-        private _vcr: ViewContainerRef,
-        private _templateRef: TemplateRef<NgLetContext>
-    ) { }
+  /**
+   * @ignore
+   */
+  constructor(
+    private _vcr: ViewContainerRef,
+    private _templateRef: TemplateRef<NgLetContext>
+  ) { }
 
-    ngOnInit() {
-        this._vcr.createEmbeddedView(
-            this._templateRef,
-            this._context
-        );
-    }
+  /**
+   * @ignore
+   */
+  ngOnInit() {
+    this._vcr.createEmbeddedView(
+      this._templateRef,
+      this._context
+    );
+  }
 }
--- projects/angular/directives/src/ui-scroll-into-view/ui-scroll-into-view.directive.ts
+++ projects/angular/directives/src/ui-scroll-into-view/ui-scroll-into-view.directive.ts
@@ -11,13 +11,33 @@ import scrollIntoViewIfNeeded from 'scroll-into-view-if-needed';
 
 type Boundary = 'parent' | Element;
 
+/**
+ * A directive that scrolls an element into view.
+ * Depends On: [scroll-into-view-if-needed](https://www.npmjs.com/package/scroll-into-view-if-needed)
+ *
+ * The browser APIs available for scrolling to an element are very primitive, scrolls on the parents as well as the child.
+ * This behavior results in weird jumps in certain scenarios, using `scroll-into-view-if-needed` helps avoid this issue.
+ *
+ * `scroll-into-view-if-needed` allows us to better control the scroll `boundary` and much more.
+ *
+ * @export
+ */
 @Directive({
   selector: '[uiScrollIntoView]',
 })
 export class UiScrollIntoViewDirective {
+  /**
+   * The boundary of the `scroll` effect.
+   *
+   */
   @Input()
   public boundary: Boundary = 'parent';
 
+  /**
+   * Configures if the decorated element should be scrolled into view.
+   * eg: `[uiScrollIntoView]="isFocused"`
+   *
+   */
   @Input()
   public set uiScrollIntoView(condition: boolean) {
     this._zone.runOutsideAngular(() => {
@@ -41,8 +61,15 @@ export class UiScrollIntoViewDirective {
     });
   }
 
+  /**
+   * Method that scrolls to the the provided `target`.
+   *
+   */
   public scrollIntoViewIfNeeded = scrollIntoViewIfNeeded;
 
+  /**
+    * @ignore
+    */
   constructor(
     private _element: ElementRef,
     private _zone: NgZone,
