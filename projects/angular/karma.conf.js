f588ddc92074d93be805b9cab07f8af58c33d49d
Vlad Jerca <vlad.jerca@gmail.com>
2019-05-29 12:36:03 +0300

    fixup! chore: configure test with coverage task

--- projects/angular/karma.conf.js
+++ projects/angular/karma.conf.js
@@ -10,6 +10,7 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      require('karma-junit-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
@@ -23,7 +24,12 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    junitReporter: {
+      outputDir: require('path').join(__dirname, '../../coverage'),
+      outputFile: '@uipath/angular.test.xml',
+      useBrowserName: false,
+    },
+    reporters: ['progress', 'junit', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
