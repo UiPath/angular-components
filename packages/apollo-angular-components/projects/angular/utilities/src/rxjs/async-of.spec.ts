f976260a27303464d098f110b86eac80600fe4f9
Vlad Jerca <vlad.jerca@gmail.com>
2019-05-30 18:45:23 +0300

    rxjs: add specifications

---
+++ packages/apollo-angular-components/projects/angular/utilities/src/rxjs/async-of.spec.ts
@@ -0,0 +1,123 @@
+import {
+  fakeAsync,
+  tick,
+} from '@angular/core/testing';
+
+import {
+  merge,
+  of,
+} from 'rxjs';
+import {
+  finalize,
+  share,
+  shareReplay,
+  skip,
+  startWith,
+  take,
+} from 'rxjs/operators';
+
+import { asyncOf } from './async-of';
+
+interface IDelayedTarget {
+    target: string;
+}
+
+describe('Util(rxjs): asyncOf', () => {
+    it('should emit sync stream first, delayed stream (0ms) second and delayed stream (10ms) third', fakeAsync((done: DoneFn) => {
+        const noDelay = of<IDelayedTarget>({ target: 'noDelay' });
+        const delay0 = asyncOf<IDelayedTarget>({ target: 'delay0' });
+        const delay10 = asyncOf<IDelayedTarget>({ target: 'delay10' }, 10);
+
+        const emissions$ = merge(
+            delay10,
+            delay0,
+            noDelay
+        ).pipe(
+            take(3),
+            share(),
+            finalize(done)
+        );
+
+        const callbacks = {
+            noDelay: (obj: IDelayedTarget) => expect(obj.target).toEqual('noDelay'),
+            delay0: (obj: IDelayedTarget) => expect(obj.target).toEqual('delay0'),
+            delay10: (obj: IDelayedTarget) => expect(obj.target).toEqual('delay10'),
+        };
+
+        const noDelaySpy = spyOn(callbacks, 'noDelay');
+        noDelaySpy.and.callThrough();
+        const delay0Spy = spyOn(callbacks, 'delay0');
+        delay0Spy.and.callThrough();
+        const delay10Spy = spyOn(callbacks, 'delay10');
+        delay10Spy.and.callThrough();
+
+        emissions$.pipe(
+            take(1)
+        ).subscribe(callbacks.noDelay);
+
+        expect(noDelaySpy).toHaveBeenCalled();
+        expect(delay0Spy).not.toHaveBeenCalled();
+        expect(delay10Spy).not.toHaveBeenCalled();
+
+        emissions$.pipe(
+            skip(1),
+            take(1)
+        ).subscribe(callbacks.delay0);
+
+        tick();
+
+        expect(delay0Spy).toHaveBeenCalled();
+        expect(delay10Spy).not.toHaveBeenCalled();
+
+        emissions$.pipe(
+            skip(2),
+            take(1)
+        ).subscribe(callbacks.delay10);
+
+        tick(10);
+
+        expect(delay10Spy).toHaveBeenCalled();
+    }));
+
+    [100, 200, 300, 400, 500, 1000, 2000, 3000].forEach((delay) => {
+        it(`should emit the value; after ${delay}ms`, fakeAsync(() => {
+            const stream$ = asyncOf(new Object(), delay)
+                .pipe(
+                    startWith(null),
+                    shareReplay(1),
+                );
+
+            const callbacks = {
+                empty: (output: Object | null) => expect(output).toBeNull(),
+                response: (output: Object | null) => {
+                    expect(output).not.toBeNull();
+                    expect(output!.constructor).toBe(Object);
+                }
+            };
+
+            const emptySpy = spyOn(callbacks, 'empty');
+            emptySpy.and.callThrough();
+            const responseSpy = spyOn(callbacks, 'response');
+            responseSpy.and.callThrough();
+
+            stream$
+                .pipe(
+                    take(1)
+                ).subscribe(callbacks.empty);
+
+            tick(delay);
+
+            expect(emptySpy).toHaveBeenCalled();
+            expect(responseSpy).not.toHaveBeenCalled();
+
+            stream$
+                .pipe(
+                    take(1)
+                ).subscribe(callbacks.response);
+
+            tick(1);
+
+            expect(responseSpy).toHaveBeenCalled();
+        }));
+    });
+});
---
+++ packages/apollo-angular-components/projects/angular/utilities/src/rxjs/concat-join.spec.ts
@@ -0,0 +1,184 @@
+import {
+  fakeAsync,
+  tick,
+} from '@angular/core/testing';
+
+import { of } from 'rxjs';
+import {
+  catchError,
+  delay,
+  finalize,
+  timeout,
+} from 'rxjs/operators';
+
+import { concatJoin } from './concat-join';
+
+interface IPayload {
+    number: number;
+}
+
+const FIRST_DELAY = 100;
+const SECOND_DELAY = 200;
+const THIRD_DELAY = 300;
+
+const FIRST_PAYLOAD: IPayload = { number: 1 };
+const SECOND_PAYLOAD: IPayload = { number: 2 };
+const THIRD_PAYLOAD: IPayload = { number: 3 };
+
+describe('Util(rxjs): concatJoin', () => {
+    describe('Scenario: sync sources', () => {
+        it('should emit the sync source aggregate in the provided order', (done) => {
+            const first = of(FIRST_PAYLOAD);
+            const second = of(SECOND_PAYLOAD);
+            const third = of(THIRD_PAYLOAD);
+
+            const aggregate$ = concatJoin(
+                first,
+                third,
+                second,
+            );
+
+            const callbacks = {
+                success: (response: IPayload[]) => {
+                    expect(response.length).toEqual(3);
+                    const [f, t, s] = response;
+
+                    expect(f).toBe(FIRST_PAYLOAD);
+                    expect(s).toBe(SECOND_PAYLOAD);
+                    expect(t).toBe(THIRD_PAYLOAD);
+                },
+            };
+
+            const successSpy = spyOn(callbacks, 'success');
+            successSpy.and.callThrough();
+
+            aggregate$
+                .pipe(finalize(done))
+                .subscribe(callbacks.success);
+
+            expect(successSpy).toHaveBeenCalled();
+            expect(successSpy).toHaveBeenCalledTimes(1);
+        });
+    });
+
+    describe('Scenario: async sources', () => {
+        it('should emit the async source aggregate in the provided order', fakeAsync((done: DoneFn) => {
+            const first = of(FIRST_PAYLOAD).pipe(delay(100));
+            const second = of(SECOND_PAYLOAD).pipe(delay(200));
+            const third = of(THIRD_PAYLOAD).pipe(delay(300));
+
+            const aggregate$ = concatJoin(
+                first,
+                third,
+                second,
+            );
+
+            const callbacks = {
+                success: (response: IPayload[]) => {
+                    expect(response.length).toEqual(3);
+                    const [f, t, s] = response;
+
+                    expect(f).toBe(FIRST_PAYLOAD);
+                    expect(s).toBe(SECOND_PAYLOAD);
+                    expect(t).toBe(THIRD_PAYLOAD);
+                },
+            };
+
+            const successSpy = spyOn(callbacks, 'success');
+            successSpy.and.callThrough();
+
+            aggregate$
+                .pipe(finalize(done))
+                .subscribe(callbacks.success);
+
+            tick(FIRST_DELAY + SECOND_DELAY + THIRD_DELAY);
+
+            expect(successSpy).toHaveBeenCalled();
+            expect(successSpy).toHaveBeenCalledTimes(1);
+        }));
+    });
+
+    describe('Scenario: sync and async sources', () => {
+        it('should emit the sync and async aggregate in the provided order', fakeAsync((done: DoneFn) => {
+            const first = of(FIRST_PAYLOAD).pipe(delay(FIRST_DELAY));
+            const second = of(SECOND_PAYLOAD);
+            const third = of(THIRD_PAYLOAD).pipe(delay(THIRD_DELAY));
+
+            const aggregate$ = concatJoin(
+                first,
+                third,
+                second,
+            );
+
+            const callbacks = {
+                success: (response: IPayload[]) => {
+                    expect(response.length).toEqual(3);
+                    const [f, t, s] = response;
+
+                    expect(f).toBe(FIRST_PAYLOAD);
+                    expect(s).toBe(SECOND_PAYLOAD);
+                    expect(t).toBe(THIRD_PAYLOAD);
+                },
+            };
+
+            const successSpy = spyOn(callbacks, 'success');
+            successSpy.and.callThrough();
+
+            aggregate$
+                .pipe(finalize(done))
+                .subscribe(callbacks.success);
+
+            tick(FIRST_DELAY + THIRD_DELAY);
+
+            expect(successSpy).toHaveBeenCalled();
+            expect(successSpy).toHaveBeenCalledTimes(1);
+        }));
+    });
+
+    describe('Scenario: timeout', () => {
+        it('should not emit before the timeout interval if a source takes too long to respond', fakeAsync((done: DoneFn) => {
+            const TIMEOUT = 4000;
+
+            const first = of(FIRST_PAYLOAD).pipe(delay(FIRST_DELAY));
+            const second = of(SECOND_PAYLOAD);
+            const third = of(THIRD_PAYLOAD).pipe(delay(TIMEOUT + THIRD_DELAY));
+
+            const aggregate$ = concatJoin(
+                first,
+                third,
+                second,
+            );
+
+            const callbacks = {
+                error: (err: Error) => {
+                    expect(err).toBeDefined();
+                    expect(err.name).toEqual('TimeoutError');
+                    return of([]);
+                },
+                success: (response: IPayload[]) => {
+                    expect(response.length).toEqual(0);
+                },
+            };
+
+            const errorSpy = spyOn(callbacks, 'error');
+            errorSpy.and.callThrough();
+            const successSpy = spyOn(callbacks, 'success');
+            successSpy.and.callThrough();
+
+            aggregate$
+                .pipe(
+                    timeout(TIMEOUT),
+                    catchError(callbacks.error),
+                    finalize(done),
+                )
+                .subscribe(callbacks.success);
+
+            tick(TIMEOUT);
+
+            expect(errorSpy).toHaveBeenCalled();
+            expect(errorSpy).toHaveBeenCalledTimes(1);
+            expect(successSpy).toHaveBeenCalled();
+            expect(successSpy).toHaveBeenCalledTimes(1);
+        }));
+    });
+});
