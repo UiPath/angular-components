3665801efdd18de0454a98fd68ac6009474c6bbd
Vlad Jerca <vlad.jerca@gmail.com>
2019-05-30 18:45:23 +0300

    rxjs: add specifications

--- projects/angular/utilities/src/rxjs/async-of.ts
+++ projects/angular/utilities/src/rxjs/async-of.ts
@@ -8,8 +8,9 @@ import { delay } from 'rxjs/operators';
  * Static operator to avoid breaking the stream when using catchError.
  *
  * @param obj  Object that will be asynchronously returned.
+ * @param [delay=0] The delay applied to the stream until it emits the provided value;
  * @returns The delayed stream.
  */
-export function asyncOf<T>(obj: T): Observable<T> {
-  return of(obj).pipe(delay(0));
+export function asyncOf<T>(obj: T, delayMs = 0): Observable<T> {
+  return of(obj).pipe(delay(delayMs));
 }
---
+++ projects/angular/utilities/src/rxjs/repeat-stream.spec.ts
@@ -0,0 +1,57 @@
+import {
+  fakeAsync,
+  tick,
+} from '@angular/core/testing';
+
+import { of } from 'rxjs';
+import {
+  finalize,
+  take,
+} from 'rxjs/operators';
+
+import { repeatStream } from './repeat-stream';
+
+const REPEAT_COUNT_LIST = Array(10)
+    .fill(void 0)
+    .map((_, idx) => (idx + 1) * 13);
+
+const REPEAT_INTERVAL_LIST = Array(5)
+    .fill(void 0)
+    .map((_, idx) => (idx + 1) * 1337);
+
+describe('Util(rxjs): repeatStream', () => {
+    let emissionCount: number;
+
+    beforeEach(() => {
+        emissionCount = 0;
+    });
+
+    REPEAT_COUNT_LIST
+        .forEach(repeat => {
+            REPEAT_INTERVAL_LIST
+                .forEach(interval => {
+                    it(`should ${repeat} times at an interval of ${interval}`, fakeAsync((done: DoneFn) => {
+                        const callbacks = {
+                            emission: (count: number) => expect(count).toEqual(emissionCount),
+                        };
+
+                        const emissionSpy = spyOn(callbacks, 'emission');
+                        emissionSpy.and.callThrough();
+
+                        repeatStream(() => of(++emissionCount), interval)
+                            .pipe(
+                                take(repeat),
+                                finalize(done)
+                            )
+                            .subscribe(callbacks.emission);
+
+                        for (let i = 0; i < repeat; i++) {
+                            tick(interval);
+                        }
+
+                        expect(emissionSpy).toHaveBeenCalled();
+                        expect(emissionSpy).toHaveBeenCalledTimes(repeat);
+                    }));
+                });
+        });
+});
