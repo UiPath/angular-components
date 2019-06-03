ed2b69cbfd4562153ddb9d3884b5128c3f2920bf
Vlad Jerca <vlad.jerca@gmail.com>
2019-06-03 13:12:56 +0300

    chore: configure commitlint scopes

--- packages/apollo-angular-components/commitlint.config.js
+++ packages/apollo-angular-components/commitlint.config.js
@@ -1,3 +1,23 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
+    rules: {
+        'scope-enum': () => {
+            const scopeList = [
+                'grid',
+                'suggest',
+                'dateformat',
+                'secondformat',
+                'autofocus',
+                'click-outside',
+                'clipboard',
+                'drag-and-drop-file',
+                'ng-let',
+                'scroll-into-view',
+                'virtual-scroll-range-loader',
+                'virtual-scroll-viewport-resize',
+            ];
+
+            return [2, 'always', scopeList];
+        }
+    },
 };
