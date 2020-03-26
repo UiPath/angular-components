1b4813ec18d1d1dd01abbe30c03a114840a23ed2
Alexandru Georoceanu <alex@navigheaza.ro>
2020-03-26 18:02:56 +0200

    fix(secondformat): change behavior to average & ISO8601

--- projects/angular/directives/ui-secondformat/src/ui-secondformat.module.ts
+++ projects/angular/directives/ui-secondformat/src/ui-secondformat.module.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { UiSecondFormatDirective } from './ui-secondformat.directive';
 
 @NgModule({
+    imports: [CommonModule, MatTooltipModule],
     declarations: [UiSecondFormatDirective],
     exports: [UiSecondFormatDirective],
 })
