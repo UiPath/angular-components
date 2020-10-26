5166ac6140f0448823ea4c59ac5fb557aef46bd8
Andrei Balasescu <andrei.balasescu@uipath.com>
2020-10-26 14:58:28 +0200

    fix(grid): expose translation method for checkbox message
    - render tooltip for grid checkboxes

--- projects/angular/components/ui-grid/src/ui-grid.component.ts
+++ projects/angular/components/ui-grid/src/ui-grid.component.ts
@@ -112,7 +112,7 @@ export class UiGridComponent<T extends IGridDataEntry> extends ResizableGrid<T> 
      *
      */
     public get isEveryVisibleRowChecked() {
-        return this.dataManager.length &&
+        return !!this.dataManager.length &&
             this.dataManager.every(row => this.selectionManager.isSelected(row!));
     }
 
@@ -638,8 +638,21 @@ export class UiGridComponent<T extends IGridDataEntry> extends ResizableGrid<T> 
     }
 
     /**
-     * Determines the `checkbox` `aria-label`.
+     * Determines the `checkbox` `matToolTip`.
      *
+     * @param [row] The row for which the label is computed.
+     */
+    public checkboxTooltip(row?: T): string {
+        if (!row) {
+            return this.intl.checkboxTooltip(this.isEveryVisibleRowChecked);
+        }
+
+        return this.intl.checkboxTooltip(this.selectionManager.isSelected(row), this.dataManager.indexOf(row));
+    }
+
+    /**
+     * Determines the `checkbox` aria-label`.
+     * **DEPRECATED**
      * @param [row] The row for which the label is computed.
      */
     public checkboxLabel(row?: T): string {
