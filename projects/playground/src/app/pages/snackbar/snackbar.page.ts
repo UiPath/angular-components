d03b8a0120102934a412f89aeb7937dab24ab5df
Cristian <cristian.calina@uipath.com>
2021-04-28 17:39:41 +0300

    fix(snackbar): correctly map icons to type & playground

---
+++ projects/playground/src/app/pages/snackbar/snackbar.page.ts
@@ -0,0 +1,25 @@
+import {
+    ICON_MAP,
+    SnackBarType,
+    UiSnackBarService,
+} from 'projects/angular/components/ui-snackbar/src/ui-snackbar.component';
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-snackbar',
+    templateUrl: './snackbar.page.html',
+    styleUrls: ['snackbar.page.scss'],
+})
+export class SnackbarPageComponent {
+    public readonly IconMap = ICON_MAP;
+    public readonly iconType = [...ICON_MAP.keys()];
+
+    constructor(
+        private _snack: UiSnackBarService,
+    ) { }
+
+    openSnackbar(snackbarType: SnackBarType.Error | SnackBarType.Info | SnackBarType.Success | SnackBarType.Warning) {
+        this._snack[snackbarType](`This is ${snackbarType}`);
+    }
+}
