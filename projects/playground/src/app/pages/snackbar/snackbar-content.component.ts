ce98939f62fbf75680641eec21a4c8d6fb7e2e10
Mihai Agape <mh.agape@gmail.com>
2021-12-20 10:34:27 +0200

    feat(snackbar): add component support

---
+++ projects/playground/src/app/pages/snackbar/snackbar-content.component.ts
@@ -0,0 +1,38 @@
+import { CommonModule } from '@angular/common';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    Inject,
+    NgModule,
+} from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { UI_MAT_SNACK_BAR_PAYLOAD } from 'projects/angular/components/ui-snackbar/src/public_api';
+
+export type SnackbarContentPayload = {
+    buttonLabel: string;
+};
+
+@Component({
+    selector: 'ui-snackbar-content',
+    template: `
+    Snackbar w/ <b>Component</b>
+    <button mat-flat-button style="margin-left: 5px;">{{payload.buttonLabel}}</button>
+    `,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SnackbarContentComponent {
+    constructor(
+        @Inject(UI_MAT_SNACK_BAR_PAYLOAD)
+        public payload: SnackbarContentPayload,
+    ) { }
+}
+
+@NgModule({
+    imports: [
+        CommonModule,
+        MatButtonModule,
+    ],
+    declarations: [SnackbarContentComponent],
+    exports: [SnackbarContentComponent],
+})
+export class SnackbarContentModule { }
