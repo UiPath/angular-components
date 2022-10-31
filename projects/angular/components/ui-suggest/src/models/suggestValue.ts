d6761a262456a696df1d5ae10f6f429f6d3d9caf
Anatolie Darii <anatolie.darii@gmail.com>
2022-10-31 17:30:27 +0200

    feat(suggest): rename preventSelection to disabled

--- projects/angular/components/ui-suggest/src/models/suggestValue.ts
+++ projects/angular/components/ui-suggest/src/models/suggestValue.ts
@@ -41,9 +41,9 @@ export interface ISuggestValue extends VirtualScrollItem {
      */
     expandable?: boolean;
     /**
-     * Flag that marks if item is non-selectable.
+     * Flag that marks if item is disabled.
      */
-     preventSelection?: boolean;
+     disabled?: boolean;
     /**
      * Optional icon that will be displayed to the left of the item.
      *
