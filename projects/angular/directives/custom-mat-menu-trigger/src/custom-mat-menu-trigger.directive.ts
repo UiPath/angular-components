1d4f552ae62474a84f80c3a6456293be44ea0ce1
Catalin <catalin.bajenaru@uipath.com>
2023-03-07 20:24:06 +0200

    feat(a11y): add menu-trigger directive that sets aria-expanded

---
+++ projects/angular/directives/custom-mat-menu-trigger/src/custom-mat-menu-trigger.directive.ts
@@ -0,0 +1,68 @@
+import {
+    Subject,
+    takeUntil,
+} from 'rxjs';
+
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import {
+    AfterContentInit,
+    Directive,
+    HostBinding,
+    inject,
+    Input,
+    OnDestroy,
+} from '@angular/core';
+import { _MatMenuTriggerBase } from '@angular/material/menu';
+
+@Directive({
+    selector: '[uiCustomMatMenuTriggerFor]',
+})
+export class UiCustomMatMenuTriggerDirective extends _MatMenuTriggerBase implements AfterContentInit, OnDestroy {
+    @HostBinding()
+    ariaExpanded = false;
+
+    @Input()
+    expandedTranslation = 'expanded';
+
+    get nativeElement() {
+        // eslint-disable-next-line no-underscore-dangle
+        return (this as any)._element.nativeElement as HTMLElement;
+    }
+
+    private _liveAnnouncer = inject(LiveAnnouncer);
+    private _destroyed$ = new Subject<void>();
+    ngAfterContentInit() {
+        this.menuOpened.pipe(
+            takeUntil(this._destroyed$),
+        ).subscribe(() => {
+            // although setting aria-expanded to true, it is not announced
+            this.nativeElement.setAttribute('aria-expanded', 'true');
+            this._liveAnnouncer.announce(this.expandedTranslation);
+
+            // after closing the menu it would appear for a short time as expanded
+            // therefore we set it as collapsed before it is actually closed
+            setTimeout(() => {
+                this.nativeElement.setAttribute('aria-expanded', 'false');
+            }, 100);
+        });
+
+        this.menuClosed.pipe(
+            takeUntil(this._destroyed$),
+        ).subscribe(() => {
+            this.nativeElement.setAttribute('aria-expanded', 'false');
+
+            // timeout is required because
+            // after focusing another element then returning to this one, it will lose collapsed state
+            setTimeout(() => {
+                this.nativeElement.setAttribute('aria-expanded', 'false');
+            }, 100);
+        });
+    }
+
+    ngOnDestroy() {
+        super.ngOnDestroy();
+        this._destroyed$.next();
+        this._destroyed$.complete();
+    }
+}
+
---
+++ projects/angular/directives/custom-mat-menu-trigger/src/custom-mat-menu-trigger.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+import { UiCustomMatMenuTriggerDirective } from './custom-mat-menu-trigger.directive';
+
+@NgModule({
+    declarations: [UiCustomMatMenuTriggerDirective],
+    exports: [UiCustomMatMenuTriggerDirective],
+})
+export class UiCustomMatMenuTriggerModule { }
