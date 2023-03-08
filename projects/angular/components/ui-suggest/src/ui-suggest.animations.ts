06f5bbf3586a0dc028a4f858961ea57a108cb23e
ZoicanDenisUI <denis.zoican@uipath.com>
2023-03-08 09:51:40 +0200

    feat(suggest): use CDK Layout to display the dropdown

--- projects/angular/components/ui-suggest/src/ui-suggest.animations.ts
+++ projects/angular/components/ui-suggest/src/ui-suggest.animations.ts
@@ -19,18 +19,23 @@ export const UI_SUGGEST_ANIMATIONS: {
     readonly transformMenuList: AnimationTriggerMetadata;
 } = {
     transformMenuList: trigger('displayState', [
-        state('closed', style({
-            opacity: 0,
-            transform: 'scale(0.8)',
-        })),
-        transition('closed => open', group([
-            animate('100ms linear', style({
-                opacity: 1,
-            })),
-            animate('120ms cubic-bezier(0, 0, 0.2, 1)', style({
-                transform: 'scale(1)',
-            })),
-        ])),
-        transition('* => closed', animate('100ms 25ms linear', style({ opacity: 0 }))),
+        state(
+            'void',
+            style({
+                opacity: 0,
+                transform: 'scale(0.8)',
+            }),
+        ),
+        transition(
+            'void => open',
+            group([
+                animate('100ms linear', style({
+                    opacity: 1,
+                })),
+                animate('120ms cubic-bezier(0, 0, 0.2, 1)', style({
+                    transform: 'scale(1)',
+                })),
+            ]),
+        ),
     ]),
 };
